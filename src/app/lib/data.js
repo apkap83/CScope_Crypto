b8b44d2ed7289bfb6e9128dc82ac54e281a6527d
apkap83 <33512036+apkap83@users.noreply.github.com>
2024-03-20 18:23:16 +0200

    Server Component rendering for fetchCoinsList

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -7,19 +7,47 @@ export async function fetchCoinsList(query, currentPage) {
   try {
     // 1 sec artificial delay to show the Loading state
     await new Promise((res) => setTimeout(res, 1000));
-
-    const urlSuffix = "http://localhost:3000/api";
-
     const ITEMS_PER_PAGE = 12;
-    const url = `${urlSuffix}?pageNumber=${currentPage}&rowsPerPage=${ITEMS_PER_PAGE}&fields=name,symbol,current_price,high_24h,low_24h,price_change_percentage_24h&query=${query}`;
 
+    // const urlPrefix = "http://localhost:3000/api";
+    // const url = `${urlPrefix}?pageNumber=${currentPage}&rowsPerPage=${ITEMS_PER_PAGE}&fields=name,symbol,current_price,high_24h,low_24h,price_change_percentage_24h&query=${query}`;
+
+    const apiAddress = process.env.COIN_GECKO_API_URL;
+    const apiKey = process.env.COIN_GECKO_API_KEY;
+    const currency = "usd";
+    // const rowsPerPage = searchParams.get("rowsPerPage");
+    // const pageNumber = searchParams.get("pageNumber");
+    const fields =
+      "name,symbol,current_price,high_24h,low_24h,price_change_percentage_24h";
+    query = query.toLowerCase();
+
+    const url = `${apiAddress}/coins/markets?vs_currency=${currency}&per_page=${ITEMS_PER_PAGE}&page=${currentPage}&ids=${query}&x_cg_demo_api_key=${apiKey}`;
+
+    console.log("url", url);
     console.log("Request Towards:", url);
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error("Failed to fetch data");
     }
-    const jsonData = await response.json();
-    return jsonData;
+    let data = await response.json();
+
+    // If fields query parameter is provided, filter the data to include only specified fields
+    if (fields) {
+      const selectedFields = fields.split(","); // Splitting fields by comma
+      data = data.map((item) => {
+        const filteredItem = {};
+        filteredItem["image"] = item["image"];
+        filteredItem["id"] = item["id"];
+        selectedFields.forEach((field) => {
+          if (item.hasOwnProperty(field)) {
+            filteredItem[field] = item[field];
+          }
+        });
+        return filteredItem;
+      });
+    }
+
+    return data;
   } catch (error) {
     console.error("API Error:", error);
     throw new Error("Failed to fetch coin list data.");
